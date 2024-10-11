84a3da5faecee780949507cdde0afbdd4de7d8d9
Ulug'bek <119843506+ulugbekhusainovv@users.noreply.github.com>
2024-10-11 11:42:00 +0500

    Temporary changes

--- account/serializers.py
+++ account/serializers.py
@@ -138,6 +138,7 @@ class TelegramOauth2Serializer(serializers.Serializer):
     hash = serializers.CharField()
 
 
+<<<<<<< Updated upstream
 class UserProfileSerializer(serializers.ModelSerializer):
     photo = MediaURlSerializer(read_only=True)
 
@@ -241,3 +242,6 @@ class UserIntroQuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserIntroQuestion
         fields = ("id", "intro_questions", "answer", "is_marked")
+=======
+
+>>>>>>> Stashed changes
